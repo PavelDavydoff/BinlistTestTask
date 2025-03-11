ba1d8bbdb678dac68266fd6a8274acc0326fcaf9
Pavel_Davydov <david20506@yandex.ru>
2025-03-11 14:12:29 +0500

    Создал граф навигации и BottomNavigation. Вёрстка фрагментов

--- app/src/main/java/com/example/binlisttesttask/RootActivity.kt
+++ app/src/main/java/com/example/binlisttesttask/RootActivity.kt
@@ -1,10 +1,9 @@
 package com.example.binlisttesttask
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.binlisttesttask.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
@@ -13,5 +12,11 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/binlisttesttask/history/HistoryFragment.kt
+++ app/src/main/java/com/example/binlisttesttask/history/HistoryFragment.kt
@@ -1,6 +1,27 @@
 package com.example.binlisttesttask.history
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.binlisttesttask.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHistoryBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
